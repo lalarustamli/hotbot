577deedb422885a29bce0d24a8b4cc2c719d4d55
lalarustamli <rustamlilala@gmail.com>
2020-07-27 10:01:58 +0400

    init

--- hotbot/routes.py
+++ hotbot/routes.py
@@ -1,6 +1,8 @@
 from hotbot import app
 from flask import request
 
+TOKEN = "EAAEFyjOzpW0BALLnIubQe3tMmM2EEhLgZAum0mqCddLwlWoWTgle0zPuwAolv9OJsebnAsg6JRFFKzaTcirSFlM5YZAHC0fwQXJZCWZArKdGoQ9okaIDrLLHe5FjAGPFcbZClNNOYDPyxw6CK2wmCgN2VbB0PYufGJNaaLkbbB5bWSLs2ZCBPR"
+API = "https://graph.facebook.com/v7.0/me/messages"
 @app.route('/')
 def index():
     return 'Hello World!'
@@ -14,10 +16,6 @@ def verify_webhook(req):
     else:
         return "wrong token"
 
-def is_user_message(event):
-    """Return whether the message is from the user.
-    """
-    return (event.get('message') and not event['message'].get("is_echo"))
 
 @app.route('/hotbot',methods=['GET', 'POST'])
 def listen():
@@ -25,10 +23,49 @@ def listen():
         return verify_webhook(request)
     if request.method == "POST":
         message = request.get_json()
-        print (message)
-        return message
+        if 'messaging' in request.json['entry'][0]:
+            events = request.json['entry'][0]['messaging']
+            for event in events:
+                if is_user_message(event):
+                    respond(event)
 
 
 
 
+def is_user_message(event):
+    """Return whether the message is from the user.
+    """
+    return (event.get('message') and not event['message'].get("is_echo"))
 
+
+def respond(event):
+    """Formulate a response to the user and pass it on to a send_message function.
+    """
+    sender_id = event['sender']['id']
+    message = "Hello you back!"
+    send_message(sender_id, message)
+
+def send_message(recipient_id, message):
+    """Send a response to Facebook by making a post request.
+    """
+    payload = {
+        'message': {
+            'text': message
+        },
+        'recipient': {
+            'id': recipient_id
+        },
+        'notification_type': 'regular'
+    }
+
+    auth = {
+        'access_token': TOKEN
+    }
+
+    response = requests.post(
+        API,
+        params=auth,
+        json=payload
+    )
+    print('payload2'+str(payload))
+    return response.json()
